fe904aefa117e840a021dd235d51af7c0e74ceaa
wcolts2000 <wcolts2000@gmail.com>
2019-01-15 12:10:48 -0800

    server connection showing on postman

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const helmet = require('helmet');
+const morgan = require('morgan');
+const knex = require('knex');
+
+const dbConfig = require('./knexfile');
 
 const server = express();
 
+const db = knex(dbConfig.development)
+
+
 server.use(express.json());
+server.use(morgan('short'));
 server.use(helmet());
 
 // endpoints here
+
+// sanity check
+server.get('/', (req, res) => {
+  res.send("api working");
+});
+
+
 
 const port = 3300;
 server.listen(port, function() {
