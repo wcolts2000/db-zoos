fe9d3188f717e801ef5d9060406971f063344529
wcolts2000 <wcolts2000@gmail.com>
2019-01-15 12:28:07 -0800

    get and post endpoints operating, will add extra error checking afteer other endpoints are added

--- index.js
+++ index.js
@@ -21,7 +21,42 @@ server.get('/', (req, res) => {
   res.send("api working");
 });
 
+// add zoo
+server.post('/api/zoos', (req, res) => {
+  db('zoos')
+    .insert(req.body)
+    .then(ids => {
+      db('zoos')
+        .where({ id: ids[0]})
+        .then(zoo => {
+          res.status(201).json(zoo)
+        });
+    })
+    .catch(err => res.status(500).json(err));
+})
 
+// list zoos
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos)
+    })
+    .catch(err => res.status(500).json(err))
+})
+
+// single zoo
+server.get('/api/zoos/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id})
+    .then(zoo => {
+      if(zoo.length) {
+        res.status(200).json(zoo)
+      } else {
+        res.status(404).json({ message: "No zoo found with that id"})
+      }
+    })
+    .catch(err => res.status(500).json(err))
+})
 
 const port = 3300;
 server.listen(port, function() {
